4d4291278e88865b50b0e3ceee0fbbd1e50cda49
nodariVadatchkoria <nodar.vadachkoria@gmail.com>
2023-05-25 02:01:57 +0400

    added top movie poster from api

--- src/app/layout/slider/slider.component.ts
+++ src/app/layout/slider/slider.component.ts
@@ -1,38 +1,34 @@
-import {Component, Input} from '@angular/core';
+import {Component, inject, Input} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {HomeComponent} from "../../pages/home/home.component";
 import {Movie} from "../../interfaces";
 import {ImagePipe} from "../../core/pipes";
+import {ButtonComponent} from "../button/button.component";
 
 @Component({
   selector: 'app-slider',
   standalone: true,
-  imports: [CommonModule, HomeComponent, ImagePipe],
+  imports: [CommonModule, HomeComponent, ImagePipe, ButtonComponent],
   templateUrl: './slider.component.html',
   styleUrls: ['./slider.component.scss']
 })
 export class SliderComponent {
+  imagePipe: ImagePipe =  new ImagePipe();
 
-  popularMovies: Movie.Popular = {} as Movie.Popular;
+  _Movies: Movie.Popular = {} as Movie.Popular;
   @Input({required: true})
-  set movies(movie: Movie.Popular | null) {
+  set popularMovies(movie: Movie.Popular | null) {
     if(!movie) {
       return;
     }
-    this.popularMovies = {
+    this._Movies = {
       ...movie,
-      backdrop_path: this.imagePipe.transform(movie?.backdrop_path, 'w1280')
+      backdrop_path: this.imagePipe.transform(movie.backdrop_path, 'original')
     }
   }
 
   get movie(): Movie.Popular | null{
-    return this.popularMovies;
+    return this._Movies;
   }
-
-constructor(
-private imagePipe: ImagePipe
-) {
-}
-  image = 'assets/images/img/ironman.jpg';
 
 }
